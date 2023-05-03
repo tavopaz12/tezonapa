99b95222b0aa4852551fc843735aec7a37218fa6
tavopaz <tavo.paz12.dev@hotmail.com>
2023-05-02 22:34:04 -0600

    feature: page download

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,17 +13,17 @@ export default function Home() {
       <article className="px-10 py-5 max-md:px-4">
         <section className="flex max-md:flex-col justify-between mb-8">
           <div className="w-2/4 max-md:w-full">
-            <Image className='h-full w-full object-cover' src={webp} alt="hola" />
+            <Image
+              className="h-full w-full object-cover"
+              src={webp}
+              alt="hola"
+            />
           </div>
 
           <div className="w-2/4 max-md:w-full grid place-items-center">
             <h2 className="font-bold capitalize text-xl mb-4">
               Publicación Destacada!!
             </h2>
-            <iframe
-              className="h-[25rem] max-md:h-[20rem] w-[80%] max-md:w-full border border-gray-300 p-1"
-              src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid02ZDQBvM9iVNDcu4aLpuRhs5G686yVwsYMdZTkuCQyV56L9yPiENaQhU5T6QDQWcTFl%26id%3D100076983524020&show_text=true&width=500"
-              allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
           </div>
         </section>
         <section id="servicio_comunidad">
@@ -54,14 +54,7 @@ export default function Home() {
               />
             </div>
 
-            <div className="w-[50%] grid justify-center max-md:w-full max-md:mt-5">
-              <iframe
-                aria-label="Facebook"
-                className="h-full w-[500px] max-md:w-full max-md:h-[20rem]"
-                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D100076983524020&tabs=timeline&width=500&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId"
-                allowFullScreen={true}
-                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-            </div>
+            <div className="w-[50%] grid justify-center max-md:w-full max-md:mt-5"></div>
           </div>
         </section>
       </article>
