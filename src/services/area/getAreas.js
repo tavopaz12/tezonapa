c34afec02b3edcec46ce51e3f02e5b01687c51d1
tavopaz <tavo.paz12.dev@hotmail.com>
2023-05-12 19:54:12 -0600

    feature: api rest completa

--- src/services/area/getAreas.js
+++ src/services/area/getAreas.js
@@ -1,6 +1,6 @@
 export const getAreas = async () => {
   try {
-    const res = await fetch('http://localhost:3000/api/areas')
+    const res = await fetch('https://tezonapa.vercel.app/api/areas')
 
     if (!res.ok) {
       throw new Error('Something went wrong with the request')
